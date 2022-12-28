0c91239d71e089a7a73a41bed8e649e2c9f9f82b
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-12-28 04:33:59 -0800

    work.

--- extension/index.js
+++ extension/index.js
@@ -1,13 +1,21 @@
 const fs = require('fs')
+let old__dirname = __dirname.slice(0, -10)
+if (!__dirname.endsWith('/extension')) {
+    old__dirname = __dirname
+    __dirname += '/extension'
+}
+const extFile = __dirname + '/extension.js'
+const decoder = old__dirname + '/decoder.js'
+const extBase = __dirname + '/base.js'
 
-fs.readFile('./base.js', (err, data) => {
+fs.readFile(extBase, (err, data) => {
     if (err) throw err;
     data = String(data)
-    const parser = fs.readFileSync('./decoder.js')
+    const parser = fs.readFileSync(decoder)
     const ext = Buffer.from(parser + '\n\n\n' + data)
-    fs.writeFileSync('./extension.js', ext)
-    fs.readFile('./extension.js', (err, data) => {
+    fs.writeFileSync(extFile, ext)
+    fs.readFile(extFile, (err, data) => {
         if (err) throw err
-        fs.writeFileSync('./extension.js', '// data:text/javascript;base64,' + ext.toString('base64') + '\n\n' + String(data))
+        fs.writeFileSync(extFile, '// data:text/javascript;base64,' + ext.toString('base64') + '\n\n' + String(data))
     })
 })
