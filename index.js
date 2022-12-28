9123e8cae041315f5f1e32574724f3bd2544f50c
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-12-27 18:45:50 -0800

    itsa me extension

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
+// for testing the decoder
 const decoder = require('./decoder')
-const { run } = require('./interpreter')
 
 console.log(decoder("$(\"relative moveable vertically\")imp,tick$=(){u>/u,/u},t:$()tick"))
