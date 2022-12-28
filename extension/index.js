b04100d6620136908510d8191e3ea41f88b48aeb
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-12-28 04:20:31 -0800

    idk at this pouint

--- extension/index.js
+++ extension/index.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const path = require('path')
 __dirname = path.resolve(__dirname, 'extensions')
 
-fs.readFile(path.resolve('base.js'), (err, data) => {
+fs.readFile(path.resolve(__dirname, 'base.js'), (err, data) => {
     if (err) throw err;
     data = String(data)
     const parser = fs.readFileSync('./decoder.js')
     const ext = Buffer.from(parser + '\n\n\n' + data)
-    fs.writeFileSync(path.resolve('extension.js'), ext)
-    fs.readFile(path.resolve('extension.js'), (err, data) => {
+    fs.writeFileSync(path.resolve(__dirname, 'extension.js'), ext)
+    fs.readFile(path.resolve(__dirname, 'extension.js'), (err, data) => {
         if (err) throw err
-        fs.writeFileSync(path.resolve('extension.js'), '// data:text/javascript;base64,' + ext.toString('base64') + '\n\n' + String(data))
+        fs.writeFileSync(path.resolve(__dirname, 'extension.js'), '// data:text/javascript;base64,' + ext.toString('base64') + '\n\n' + String(data))
     })
 })
