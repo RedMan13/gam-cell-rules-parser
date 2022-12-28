50a79148f664aeb80f5a24019336131dded98968
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-12-27 18:48:25 -0800

    b

--- decoder.js
+++ decoder.js
@@ -119,7 +119,7 @@ const parseCommand = (com) => {
   return singles[com] ? singles[com] : singles['na']
 }
 
-module.exports = function(rule) {
+const parser = function(rule) {
   rule = rule.replaceAll('\n', '')
   let inside = false
   let commands = ['']
@@ -140,3 +140,50 @@ module.exports = function(rule) {
   }
   return commands.map(parseCommand)
 }
+
+module.exports = parser
+
+class cellParser {
+	constructor (runtime) {
+		// Extension stuff
+		this.runtime = runtime;
+	}
+
+	getInfo () {
+		return {
+			id: 'cellparser',
+			name: 'Gam Cell Rule Parser',
+			color1: '#4576b4d',
+			blocks: [
+				{
+					opcode: 'parsetextcodeintojson',
+					blockType: "reporter",
+					text: "parse text code [text] into json data",
+					arguments: {
+						text: {
+							type: "string",
+							defaultValue: "$(\"relative moveable vertically\")imp,tick$=(){u>/u,/u},t:$()tick"
+						}
+					}
+				}
+			]
+		};
+	};
+	
+	// Code for blocks go here
+	
+    parsetextcodeintojson({text}) {
+        return JSON.stringify(parser(text))
+    }
+};
+
+(function() {
+	var extensionClass = cellParser;
+	if (typeof window === "undefined" || !window.vm) {
+		Scratch.extensions.register(new extensionClass());
+	} else {
+		var extensionInstance = new extensionClass(window.vm.extensionManager.runtime);
+		var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance);
+		window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
+	};
+})()
